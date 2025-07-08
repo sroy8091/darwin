efbacd51fd72ef7b030993977555689678fabfec
Sumit Roy <sroy8091@gmail.com>
2025-07-08 17:02:22 +0530

    Add ngrok to test in local

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => {
       server: {
         host: '0.0.0.0',
         port: 5173,
-        allowedHosts: ['08508de43105.ngrok-free.app']
+        allowedHosts: ['.ngrok-free.app']
       }
     };
 });
